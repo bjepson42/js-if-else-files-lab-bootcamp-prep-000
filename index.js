3631b33c308ec0037dd8593f2fcb0d5b13e5eba5
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-11-05 20:00:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var name = "Susan"
 var height = "74"
 
-
 if (name === "Susan"){
-console.log("The name is correct")
+    console.log("The name is correct")
   } else {
     console.log("Expected the name Susan")
   }
